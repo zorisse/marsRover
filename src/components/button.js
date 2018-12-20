cbda7aa20859b3b966e23f9be1e6838b610cc624
Nouri T <zorisse@gmail.com>
2018-12-20 11:14:56 +0100

    un peu de style

--- src/components/button.js
+++ src/components/button.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+const styleButton ={
+    fontFamily:'Orbitron',
+    margin: '8px',
+    padding: '8px',
+}
+
 
 const Buttons = (props) =>{
     return(
-        <div style={{display:'flex', flexDirection:'row', justifyContent:'center', margin:'20px 0px'  }}>
-    <button  onClick={props.turnLeft} >turnLeft</button>
-    <button  onClick={props.turnRight} >turnRight</button>
-    <button  onClick={props.moveFoward} >Move Forward</button>
+     <div style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
+    <button style={styleButton} onClick={props.turnLeft} >turnLeft</button>
+    <button style={styleButton} onClick={props.turnRight} >turnRight</button>
+    <button style={styleButton} onClick={props.moveFoward} >Move Forward</button>
         </div>
     )
 }
